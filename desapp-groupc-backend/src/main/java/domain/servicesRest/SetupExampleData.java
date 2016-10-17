c72a1f9c63b4e1a29b65c314de64afdad7044258
franciolucio <franciolucio@gmail.com>
2016-10-17 19:36:34 -0300

    servicios basicos para la entrega terminados.

--- desapp-groupc-backend/src/main/java/domain/servicesRest/SetupExampleData.java
+++ desapp-groupc-backend/src/main/java/domain/servicesRest/SetupExampleData.java
@@ -13,6 +13,7 @@ import domain.builders.ProfileBuilder;
 import domain.exceptions.SingUpException;
 import domain.services.EventService;
 import domain.services.UserService;
+import domain.types.Type;
 
 
 public class SetupExampleData {
@@ -43,14 +44,32 @@ public class SetupExampleData {
     @Transactional
     public void init() throws SingUpException {
     	User user = userService.signUp("franciolucio","unqui","franciolucio@gmail.com");
-    	Profile profile = ProfileBuilder.aProfile().build();
+    	Profile profile = ProfileBuilder.aProfile()
+    					  .withLimitAmount(500)
+    					  .withTypeOfFilm(Type.ACTION)
+    					  .withTypeOfFood(Type.PIZZA)
+    					  .withTypeOfMusic(Type.ELECTRONIC)
+    					  .build();
     	user.setFriends(new ArrayList<User>());
     	this.userService.addProfileForUser(user, profile);
     	User friend01 = new User("emimancu","sanlorenzo","emimancu@gmail.com");
     	User friend02 = new User("alanmarino","rockandroll","alanmarino@gmail.com");
+    	User friend03 = new User("martinez","1234","martinez@gmail.com");
+    	User friend04 = new User("lopez","12345","lopez@gmail.com");
+    	User friend05 = new User("juanPablo","123456","juanPablo@gmail.com");
+    	User friend06 = new User("juanMartin","1234567","juanMartin@gmail.com");
+    	User friend07= new User("ivanFernandez","12345678","ivanFernandez@gmail.com");
+    	User friend08 = new User("joseLuis12","123456789","joseLuis12@gmail.com");
+    	
     	
     	this.userService.addFriendForUser(user, friend01);
     	this.userService.addFriendForUser(user, friend02);
+    	this.userService.addFriendForUser(user, friend03);
+    	this.userService.addFriendForUser(user, friend04);
+    	this.userService.addFriendForUser(user, friend05);
+    	this.userService.addFriendForUser(user, friend06);
+    	this.userService.addFriendForUser(user, friend07);
+    	this.userService.addFriendForUser(user, friend08);
     	Event event01 = EventBuilder.aEvent().withAddress("Pilar").build();
     	Event event02 = EventBuilder.aEvent().withAddress("Castelar").build();
     	Event event03 = EventBuilder.aEvent().withAddress("Villa Crespo").build();
